d7fac4a0887e5b15521f09503af8caa80758e12f
Federico Magnolfi <federicomagno96@gmail.com>
2021-04-28 19:22:39 +0200

    kwargs in caching

--- utils/cache.py
+++ utils/cache.py
@@ -11,11 +11,11 @@ def caching(function):
         os.makedirs(CACHE_DIR)
         print("creating cache dir")
 
-    def wrapped_function(*args):
+    def wrapped_function(*args, **kwargs):
 
         def _obj_to_hex(o):
             if callable(o):
-                return "&".join([o.__module__, o.__qualname__, str(signature(o)), str(o.__defaults__), str(o.__kwdefaults__)])
+                return "&".join([o.__module__, o.__name__, o.__qualname__, str(signature(o)), str(o.__defaults__), str(o.__kwdefaults__)])
             elif type(o) == dict:
                 return str(o)
             elif hasattr(o, "__iter__"):
@@ -26,13 +26,13 @@ def caching(function):
             else:
                 return str(o)
 
-        args_to_hash = (function.__module__, function.__name__, function.__qualname__, *args)
+        args_to_hash = (function, *args, *(kwargs.keys()), *(kwargs.values()))
         hex_digest = hashlib.sha256(bytes(str([_obj_to_hex(arg) for arg in args_to_hash]), "utf-8")).hexdigest()
 
         file_cache = os.path.join(CACHE_DIR, hex_digest)
         if not os.path.exists(file_cache):
             print(f"calculating result for function {function.__qualname__}...", end="")
-            result = function(*args)
+            result = function(*args, **kwargs)
             with open(file_cache, "wb") as file:
                 pickle.dump(result, file)
             print(f"\rcalculated result for function {function.__qualname__}    ")
